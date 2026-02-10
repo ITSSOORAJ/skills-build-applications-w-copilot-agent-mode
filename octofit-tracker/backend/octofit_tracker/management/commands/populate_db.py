2acb4981894307a35428421c4e28c4cc2fdf3ec3
SOORAJ KUMAR <121786021+ITSSOORAJ@users.noreply.github.com>
2026-02-10 08:04:57 +0000

    Django MongoDB models, API, and test data management command for OctoFit Tracker

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Workout, Leaderboard
+from django.utils import timezone
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Clear existing data
+        Leaderboard.objects.all().delete()
+        Activity.objects.all().delete()
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel', description='Marvel superheroes')
+        dc = Team.objects.create(name='DC', description='DC superheroes')
+
+        # Create users
+        users = [
+            User.objects.create(name='Spider-Man', email='spiderman@marvel.com', team=marvel),
+            User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel),
+            User.objects.create(name='Wonder Woman', email='wonderwoman@dc.com', team=dc),
+            User.objects.create(name='Batman', email='batman@dc.com', team=dc),
+        ]
+
+        # Create workouts
+        w1 = Workout.objects.create(name='Pushups', description='Do 20 pushups', suggested_for='all')
+        w2 = Workout.objects.create(name='Running', description='Run 5km', suggested_for='all')
+
+        # Create activities
+        Activity.objects.create(user=users[0], type='pushups', duration=10, date=timezone.now().date())
+        Activity.objects.create(user=users[1], type='running', duration=30, date=timezone.now().date())
+        Activity.objects.create(user=users[2], type='pushups', duration=15, date=timezone.now().date())
+        Activity.objects.create(user=users[3], type='running', duration=25, date=timezone.now().date())
+
+        # Create leaderboard
+        Leaderboard.objects.create(user=users[0], score=100, rank=1)
+        Leaderboard.objects.create(user=users[1], score=90, rank=2)
+        Leaderboard.objects.create(user=users[2], score=80, rank=3)
+        Leaderboard.objects.create(user=users[3], score=70, rank=4)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db populated with test data!'))
